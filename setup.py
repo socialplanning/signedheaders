c55d7e8b25971f14afe9eac1239e42bb7deb69b7
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-11-02 15:07:56 +0000

    update package data
    git-svn-id: https://svn.socialplanning.org/svn/signedheaders/trunk@27719 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+version = '0.1'
 
 setup(name='signedheaders',
       version=version,
-      description="Opencore signedheaders",
+      description="OpenCore signed headers",
       long_description="""\
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='David Turner',
-      author_email='novalis@openplans.org',
+      author_email='opencore-dev at lists.coactivate.org',
       url='',
       license='GPLv3 or any later version',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
